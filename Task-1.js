fab3fe2956324e5adfdc7660cbebf87bd6c26bf9
Lokeswari Muvvala <muvvalalokeswari01@gmail.com>
2025-05-26 18:23:04 +0530

    Add files via upload

---
+++ Task-1.js
@@ -0,0 +1,53 @@
+ function clickHere()
+     {
+          alert('Congrats!!!..You are Joined in WebTech');
+    }
+        
+    function Ehtml(){
+        alert('You are ENROLLED in the HTML course.');
+    }
+
+     function Ecss(){
+        alert('You are ENROLLED in the CSS course.');
+    }
+    function EJS()
+    {
+         alert('You are ENROLLED in the JAVA SCRIPT course.');
+
+    }
+    function Ereact()
+    {
+           alert('You are ENROLLED in the REACT course.');
+    }
+    function EnodeJs()
+    {
+           alert('You are ENROLLED in the nodeJs course.');
+    }
+    function EDB()
+    {
+          alert('You are ENROLLED in the DATABASE course.');
+    }
+    function EJava()
+    {
+           alert('You are ENROLLED in the Java course.');
+    }
+    function Ephy()
+    {
+           alert('You are ENROLLED in the Python course.');
+    }
+    function EmStack()
+    {
+          alert('You are ENROLLED in the MernStack course.');
+    }
+    function EBcamp()
+    {
+          alert('You are ENROLLED in the FULL STACK BOOTCAMP course.');
+    }
+    function Edev()
+     {
+          alert('You are ENROLLED in the DevOps course.');
+     }
+     function EFB()
+     {
+          alert('You are ENROLLED in the FRONTEND & BACKEND courses.');
+     }
